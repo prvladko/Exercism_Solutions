724426f59fe601990e229c048d5805d557034dd8
prvladko <prvladko@gmail.com>
2023-03-04 11:01:52 +0200

    pacman

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -5,14 +5,19 @@ defmodule FreelancerRates do
 
   def apply_discount(before_discount, discount) do
     before_discount - before_discount * discount / 100
+    # |> Float.round(5)
   end
 
   def monthly_rate(hourly_rate, discount) do
     monthly = daily_rate(hourly_rate) * 22
-    Kernel.trunc(Float.ceil(monthly - apply_discount(monthly, discount)))
+    # trunc(Float.ceil(monthly - monthly * discount / 100))
+    # apply_discount(monthly, discount) |> Float.ceil(0) |> trunc
+    trunc(Float.ceil(apply_discount(monthly, discount)))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    Float.floor(budget / (monthly_rate(hourly_rate, discount) / 22), 1)
+    # Float.floor(budget / (monthly_rate(hourly_rate, discount) / 22), 1)
+    daily_rate_cd = daily_rate(hourly_rate)
+    trunc(budget/apply_discount(daily_rate_cd, discount)*10)/10
   end
 end
--- elixir/pacman-rules/lib/rules.ex
+++ elixir/pacman-rules/lib/rules.ex
@@ -1,17 +1,17 @@
 defmodule Rules do
   def eat_ghost?(power_pellet_active, touching_ghost) do
-    # Please implement the eat_ghost?/2 function
+    power_pellet_active == true and touching_ghost == true
   end
 
   def score?(touching_power_pellet, touching_dot) do
-    # Please implement the score?/2 function
+    touching_power_pellet == true or touching_dot == true
   end
 
   def lose?(power_pellet_active, touching_ghost) do
-    # Please implement the lose?/2 function
+    power_pellet_active == false and touching_ghost == true
   end
 
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
-    # Please implement the win?/3 function
+    has_eaten_all_dots == true and lose?(power_pellet_active, touching_ghost) == false
   end
 end
