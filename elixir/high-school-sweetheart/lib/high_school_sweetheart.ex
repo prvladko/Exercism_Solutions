2ebca50569d5455350d9b7d296f6d155a52e2555
prvladko <prvladko@gmail.com>
2023-03-12 09:51:15 +0200

    High School Sweetheart refactor

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,28 +1,37 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
-    #String.first(String.trim("#{name}"))
     name
     |> String.trim()
     |> String.first()
   end
-
   def initial(name) do
-    # Please implement the initial/1 function
-    first_letter(name)
+    name
+    |> first_letter()
     |> String.upcase()
-    |> Kernel.<> "."
+    |> Kernel.<>(".")
   end
-
   def initials(full_name) do
-    # Please implement the initials/1 function
-    [n | s] = String.split(full_name)
-    initial(n) <> " " <> initial(Enum.at(s, 0))
+    [first_name, last_name] = String.split(full_name)
+    "#{initial(first_name)} #{initial(last_name)}"
   end
-
   def pair(full_name1, full_name2) do
-    pairin = initials(full_name1) <> "  +  " <> initials(full_name2)
-
-"     ******       ******\n   **      **   **      **\n **         ** **         **\n**            *            **\n**                         **\n**     #{pairin}     **\n **                       **\n   **                   **\n     **               **\n       **           **\n         **       **\n           **   **\n             ***\n              *\n"
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{i1}  +  #{i2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
